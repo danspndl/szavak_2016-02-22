630dfe6678b6a13ff8210d0d3f9be92f6142b954
Daniel Spindelbauer <spindel.daniel97@gmail.com>
2016-02-23 18:38:08 +0100

    Started t3
    Doesn’t read input from file

--- szavak/main.cpp
+++ szavak/main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  szavak
 //
-//  Created by sdaniel on 23/02/16.
+//  Created by sdaniel on 22/02/16.
 //  Copyright © 2016 Daniel Spindelbauer. All rights reserved.
 //
 
@@ -21,7 +21,7 @@ void t1(){
     
     cout<<"1. feladat"<<endl;
     cout<<"Type in a word: ";
-    cin>>userInput;
+    // cin>>userInput;
     userInput="loremipsum"; // DEBUG
     
     for(int i=0; i<userInput.length();i++){
@@ -44,42 +44,57 @@ void t2(){
     string tempStr="";
     
     // Get input from file
-    int i=0;
     while(!fileInput.eof()) {
         getline(fileInput,tempStr);
         if(tempStr.length()>maxStr.length()){
             maxStr=tempStr;
         }
-        i++;
     }
     
     cout<<"2. feladat"<<endl;
-    cout<<"Leghosszabb szo: "<<maxStr<<", hossza: "<<maxStr.length()<<endl;
+    cout<<"Leghosszabb szo: "<<maxStr<<", hossza: "<<maxStr.length()<<" karakter"<<endl;
 }
 
 void t3(){
     int mghNum=0, msgNum=0, wordCounter=0;
     string tempStr="";
-    
+   
+
+    cout<<"outside of input"<<endl;
     // Get input from file
-    int i=0;
+    int lineNum=0;
     while(!fileInput.eof()) {
+        cout<<"inside of input"<<endl;
         getline(fileInput,tempStr);
+        lineNum++;
+        cout<<tempStr<<endl;
+        char mghCond[5]={'a','e','i','o','u'};
+
+        for(int k=0; k<tempStr.length();k++){
+            char charTemp=tempStr[k];
+            for(int c=0; c<5;c++){
+                if(charTemp==mghCond[c]){
+                    mghNum++;
+                }else{
+                    msgNum++;
+                }
+            }
+        }
+        cout<<"     mgh "<<mghNum<<endl;
+        cout<<"     msg "<<msgNum<<endl;
         
-        
-        
-        
-        
-        
-        
-        i++;
+        if (mghNum>msgNum) {
+            cout<<tempStr<<" ";
+            wordCounter++;
+        }
     }
-    double percentage=0;
-    percentage= wordCounter/i;
+    cout<<lineNum<<endl;
+    int percentage=0;
+    // percentage=wordCounter/lineNum;
     
     cout<<"3. feladat"<<endl;
     cout<<"Ennyi darab: "<<wordCounter<<endl;
-    cout<<"ˆsszes darab: "<<i<<endl;
+    cout<<"Osszes darab: "<<lineNum<<endl;
     cout<<"Ennyi szazalek: "<<percentage<<endl;
 }
 
@@ -87,7 +102,7 @@ int main(){
     // Get input from file
     //  int i=0;
     //  while(!fileInput.eof()) {
-    //getline(fileInput,ip[i]);
+    //getline(fileInput,<array>[i]);
     //     i++;
     //}
     
