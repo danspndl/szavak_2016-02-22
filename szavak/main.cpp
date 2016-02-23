67c482b3ff97fb242f82592353c1cc6fe1c1ae38
Daniel Spindelbauer <spindel.daniel97@gmail.com>
2016-02-23 17:11:47 +0100

    Initial Commit

---
+++ szavak/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  szavak
+//
+//  Created by sdaniel on 23/02/16.
+//  Copyright © 2016 Daniel Spindelbauer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
